4265d47a703bb4cdcf87002d5b4f41d635123abe
mkspramuditha <mkspramuditha@gmail.com>
2016-12-13 16:58:09 +0530

    excel sheet reading started

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -4,7 +4,10 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use PHPExcel_IOFactory;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class SiteController extends DefaultController
@@ -27,5 +30,36 @@ class SiteController extends DefaultController
         return $this->render('default/index.html.twig',array(
             'items'=>$items
         ));
+    }
+
+    /**
+     * @Route("/excel", name="uploadExcel")
+     */
+    public function excelRead(Request $request){
+        $inputFileName = ($this->get('kernel')->getRootDir() .'/../web/upload/dimension.xlsx');;
+        try {
+            $inputFileType = PHPExcel_IOFactory::identify($inputFileName);
+            $objReader = PHPExcel_IOFactory::createReader($inputFileType);
+            $objPHPExcel = $objReader->load($inputFileName);
+        } catch(Exception $e) {
+            die('Error loading file "'.pathinfo($inputFileName,PATHINFO_BASENAME).'": '.$e->getMessage());
+        }
+
+        $sheet = $objPHPExcel->getSheet(0);
+        $highestRow = $sheet->getHighestRow();
+        $highestColumn = $sheet->getHighestColumn();
+
+        for ($row = 2; $row <= $highestRow; $row++){
+
+            $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
+            foreach ($rowData as $row){
+                var_dump($row."  ");
+            }
+            var_dump("\n");
+
+        }
+
+        return new Response('Hi');
+
     }
 }
